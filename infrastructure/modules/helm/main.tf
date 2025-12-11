8723507c26d113eb0c84009666747863a6839ad8
Manoj-14 <manumadhu1425@gmail.com>
2025-12-11 21:36:14 +0530

    refactor: helm_release repo updated with git repo url

--- infrastructure/modules/helm/main.tf
+++ infrastructure/modules/helm/main.tf
@@ -21,7 +21,7 @@ provider "helm" {
 
 resource "helm_release" "ingress-nginx" {
   name = "ingress-nginx"
-  repository = "oci://registry.k8s.io/ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
   namespace = "ingress-nginx"
   create_namespace = true
